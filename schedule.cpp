30f2c56fb5c4e54b97e3c59952b3eebff3adfd9a
COM\vojta <vojta.wasserbauerp@gmail.com>
2024-09-13 15:57:12 +0200

    added time marks for each class

--- schedule.cpp
+++ schedule.cpp
@@ -34,6 +34,7 @@ Schedule::Schedule(QWidget*parent): QWidget(parent)  {
     dateLay->addSpacing(350);
     table->addLayout(dateLay);
     table ->addWidget(calendar);
+    table->addStretch();
     setLayout(table);
 
     calendar->setVerticalHeaderItem(0, monday);
@@ -42,13 +43,26 @@ Schedule::Schedule(QWidget*parent): QWidget(parent)  {
     calendar->setVerticalHeaderItem(3, thursday);
     calendar->setVerticalHeaderItem(4, friday);
 
+    calendar->setHorizontalHeaderItem(0, zero);
+    calendar->setHorizontalHeaderItem(1, one);
+    calendar->setHorizontalHeaderItem(2, two);
+    calendar->setHorizontalHeaderItem(3, three);
+    calendar->setHorizontalHeaderItem(4, four);
+    calendar->setHorizontalHeaderItem(5, five);
+    calendar->setHorizontalHeaderItem(6, six);
+    calendar->setHorizontalHeaderItem(7, seven);
+    calendar->setHorizontalHeaderItem(8, eight);
+    calendar->setHorizontalHeaderItem(9, nine);
+
+
+
     calendar->setRowHeight(0, 75);
     calendar->setRowHeight(1, 75);
     calendar->setRowHeight(2, 75);
     calendar->setRowHeight(3, 75);
     calendar->setRowHeight(4, 75);
 
-    calendar->setFixedSize(1082,402);
+    calendar->setFixedSize(1086,402);
 
     calendar->setDisabled(true);
 
