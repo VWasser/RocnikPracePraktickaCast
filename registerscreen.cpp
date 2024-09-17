64e6d605b5b0ea8b689887f38dd4d6112639f334
Roman Podymov <podymfrombryansk@yandex.com>
2024-09-17 09:53:01 +0200

    Runnable (but not finished)

--- registerscreen.cpp
+++ registerscreen.cpp
@@ -16,7 +16,8 @@ registerscreen::registerscreen(QWidget *parent): QWidget(parent),
                 password2->clear();
 
             }else{
-                api->userAPI.registerUser(BackendlessRegisterUser(email->text(), name->text(), password->text()));
+                // TODO: implement your own BackendlessRegisterUser, see https://github.com/RomanPodymov/FinanceTracker/blob/main/App/registerscreen.hpp
+                //api->userAPI.registerUser(BackendlessRegisterUser(email->text(), name->text(), password->text()));
             }
 
 
