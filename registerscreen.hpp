2aa3464a5c68bc8e97707c2372d3d5c859f3ed11
Roman Podymov <podymfrombryansk@yandex.com>
2024-09-19 00:39:27 +0200

    Improved tip

--- registerscreen.hpp
+++ registerscreen.hpp
@@ -7,7 +7,25 @@
 #include <QLabel>
 #include <QBoxLayout>
 #include <QCheckBox>
+#include "BackendlessQt/BackendlessUser.hpp"
 
+struct BackendlessRegisterUser: BasicBackendlessRegisterUser {
+public:
+    BackendlessRegisterUser(
+        QString _email,
+        QString _password,
+        bool _someCustomField
+    ): BasicBackendlessRegisterUser(_email, _password), someCustomField(_someCustomField) { }
+
+    QMap<QString, QString> getAllParams() override {
+        QMap<QString, QString> result = {{"someCustomField", someCustomField ? "true" : "false"}};
+        result.insert(BasicBackendlessRegisterUser::getAllParams());
+        return result;
+    }
+
+protected:
+    bool someCustomField;
+};
 
 class registerscreen : public QWidget
 {
