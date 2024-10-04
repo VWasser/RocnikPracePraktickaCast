d72efe18f8faae3266038a38cc6138a67bb99405
COM\vojta <vojta.wasserbauerp@gmail.com>
2024-10-04 14:58:09 +0200

    lupdate

--- MyBachelor_en_GB.ts
+++ MyBachelor_en_GB.ts
@@ -4,84 +4,182 @@
 <context>
     <name>Schedule</name>
     <message>
-        <location filename="schedule.hpp" line="39"/>
+        <location filename="schedule.hpp" line="40"/>
         <source>MONDAY</source>
         <translation>CHECKING</translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="39"/>
-        <source>MONDAY</source>
-        <oldsource>MONDAY1</oldsource>
-        <translation type="unfinished">CHECKING</translation>
-    </message>
-    <message>
-        <location filename="schedule.hpp" line="40"/>
+        <location filename="schedule.hpp" line="41"/>
         <source>TUESDAY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="41"/>
+        <location filename="schedule.hpp" line="42"/>
         <source>WEDNESDAY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="42"/>
+        <location filename="schedule.hpp" line="43"/>
         <source>THURDAY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="43"/>
+        <location filename="schedule.hpp" line="44"/>
         <source>FRIDAY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="46"/>
+        <location filename="schedule.hpp" line="47"/>
         <source>7:55 - 8:40</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="47"/>
+        <location filename="schedule.hpp" line="48"/>
         <source>8:45 - 9:30</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="48"/>
+        <location filename="schedule.hpp" line="49"/>
         <source>9:40 - 10:25</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="49"/>
+        <location filename="schedule.hpp" line="50"/>
         <source>10:50 - 11:35</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="50"/>
+        <location filename="schedule.hpp" line="51"/>
         <source>11:45 - 12:30</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="51"/>
+        <location filename="schedule.hpp" line="52"/>
         <source>12:40 - 13:25</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="52"/>
+        <location filename="schedule.hpp" line="53"/>
         <source>13:30 - 14:15</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="53"/>
+        <location filename="schedule.hpp" line="54"/>
         <source>14:20 - 15:05</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="54"/>
+        <location filename="schedule.hpp" line="55"/>
         <source>15:10 - 15:55</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="schedule.hpp" line="55"/>
+        <location filename="schedule.hpp" line="56"/>
         <source>16:00 - 16:45</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SignInScreen</name>
+    <message>
+        <location filename="signinscreen.cpp" line="56"/>
+        <source>Přihlásit se</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="57"/>
+        <source>Zaregistrovat se</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="58"/>
+        <source>Resetovat Heslo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="60"/>
+        <source>Incorect Email/Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="61"/>
+        <source>Please try againg or change your password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="62"/>
+        <source>zobrazit heslo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="80"/>
+        <source>email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="signinscreen.cpp" line="81"/>
+        <source>heslo</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>editModeScreen</name>
+    <message>
+        <location filename="editmodescreen.cpp" line="10"/>
+        <source>Jméno hodiny</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editmodescreen.cpp" line="11"/>
+        <source>Hodina</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="editmodescreen.cpp" line="12"/>
+        <source>Den</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>registerscreen</name>
+    <message>
+        <location filename="registerscreen.cpp" line="26"/>
+        <source>Zaregistrovat se</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="27"/>
+        <source>Přihlásit se</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="28"/>
+        <source>jste už zaregistrováni? </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="30"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="31"/>
+        <source>Jméno</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="32"/>
+        <source>heslo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="33"/>
+        <source>heslo znovu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerscreen.cpp" line="34"/>
+        <source>Jste Učitel?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
