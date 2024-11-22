ed718dee1d56bce3072e6897d8f466b0cc1dc627
VWasser <163659688+VWasser@users.noreply.github.com>
2024-11-22 17:57:03 +0100

    new files
    files to merge with new windows

---
+++ gradeswindow.cpp
@@ -0,0 +1,5 @@
+#include "gradeswindow.hpp"
+
+gradesWindow::gradesWindow(QWidget *parent):QWidget(parent) {}
+
+gradesWindow::~gradesWindow(){}
---
+++ gradeswindow.hpp
@@ -0,0 +1,14 @@
+#ifndef GRADESWINDOW_HPP
+#define GRADESWINDOW_HPP
+
+#include <QWidget>
+
+class gradesWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    gradesWindow(QWidget *parent = nullptr);
+    ~gradesWindow();
+};
+
+#endif // GRADESWINDOW_HPP
