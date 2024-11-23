b88a465651255e26a451da0f4287c02749ee01d7
COM\vojta <vojta.wasserbauerp@gmail.com>
2024-11-23 11:40:03 +0100

    newly added windows

---
+++ gradeswindow.hpp
@@ -0,0 +1,15 @@
+#ifndef GRADESWINDOW_HPP
+#define GRADESWINDOW_HPP
+
+#include <QWidget>
+
+class gradesWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    gradesWindow(QWidget *parent = nullptr);
+    ~gradesWindow();
+};
+
+
+#endif // GRADESWINDOW_HPP
---
+++ menuwindow.hpp
@@ -0,0 +1,42 @@
+#ifndef MENUWINDOW_H
+#define MENUWINDOW_H
+
+#include "absencewindow.hpp"
+#include "gradeswindow.hpp"
+#include "qpushbutton.h"
+#include "schedule.hpp"
+#include <QWidget>
+#include <QGridLayout>
+#include <QMenuBar>
+
+extern Schedule* myWindow3;
+extern gradesWindow* gradeWin;
+extern absenceWindow* abscWin;
+
+class menuWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    menuWindow(QWidget *parent = nullptr);
+    ~menuWindow();
+
+
+private:
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QHBoxLayout *row1 = new QHBoxLayout;
+    QHBoxLayout *row2 = new QHBoxLayout;
+
+    QPushButton *scheduleButton = new QPushButton(menuWindow::tr("Schedule"));
+    QPushButton *absenceButton = new QPushButton(menuWindow::tr("Grades"));
+    QPushButton *gradesButton = new QPushButton(menuWindow::tr("Absence"));
+    QPushButton *settingsButton = new QPushButton;
+    QPushButton *LanguageButton = new QPushButton;
+
+
+public:
+    QMenuBar *quickAcces = new QMenuBar;
+
+};
+
+
+#endif // MENUWINDOW_H
