94b33e5f05b954cbdb54a6c34fed6ccb81476003
Roman Podymov <podymfrombryansk@yandex.com>
2025-02-04 20:27:38 +0100

    Remove commented code

--- coordinator.cpp
+++ coordinator.cpp
@@ -1,7 +1,5 @@
 #include "coordinator.hpp"
 #include "absencewindow.hpp"
-#include "BackendlessQt/StandardNetworkManager.hpp"
-#include "httpclient.hpp"
 #include <QObject>
 
 Coordinator::Coordinator(QObject *parent) : QObject(parent) {
@@ -22,14 +20,6 @@ Coordinator::Coordinator(QObject *parent) : QObject(parent) {
 Coordinator::~Coordinator() {}
 
 void Coordinator::showSignInScreen() {
-    /*if (!signInScreen) {
-        signInScreen = new SignInScreen();
-        QObject::connect(signInScreen, &SignInScreen::goToMenu, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showMenuWindow();
-            coordinator->signInScreen->hide();
-        });
-    }*/
     myWindow->show();
 }
 
@@ -57,73 +47,25 @@ void Coordinator::showMenuWindow() {
 }
 
 void Coordinator::showAbsenceWindow() {
-    /*if (!absenceWindow) {
-        absenceWindow = new ::absenceWindow();
-        QObject::connect(absenceWindow, &AbsenceWindow::goBackToMenu, this, [this]() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showMenuWindow();
-            coordinator->absenceWindow->hide();
-        });
-    }*/
     abscWin->show();
 }
 
 void Coordinator::showGradesWindow() {
-    /*if (!gradesWindow) {
-        gradesWindow = new GradesWindow();
-        QObject::connect(gradesWindow, &GradesWindow::goBackToMenu, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showMenuWindow();
-            coordinator->gradesWindow->hide();
-        });
-    }*/
     gradeWin->show();
 }
 
 void Coordinator::showInputAbsence() {
-    /*if (!inputAbsence) {
-        inputAbsence = new InputAbsence();
-        QObject::connect(inputAbsence, &InputAbsence::goBackToAbsence, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showAbsenceWindow();
-            coordinator->inputAbsence->hide();
-        });
-    }*/
     absencePopUp->show();
 }
 
 void Coordinator::showRegisterScreen() {
-    /*if (!registerScreen) {
-        registerScreen = new RegisterScreen();
-        QObject::connect(registerScreen, &RegisterScreen::goBackToSignIn, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showSignInScreen();
-            coordinator->registerScreen->hide();
-        });
-    }*/
     myWindow2->show();
 }
 
 void Coordinator::showSettingsWindow() {
-    /*if (!settingsWindow) {
-        settingsWindow = new SettingsWindow();
-        QObject::connect(settingsWindow, &SettingsWindow::goBackToMenu, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showMenuWindow();
-            coordinator->settingsWindow->hide();
-        });
-    }*/
     popUpWindow->show();
 }
 
 void Coordinator::showSchedule() {
-    /*if (!schedule) {
-        schedule = new Schedule();
-        QObject::connect(schedule, &Schedule::goBackToMenu, this, []() {
-            auto coordinator = static_cast<Coordinator*>(QObject::sender()->parent());
-            coordinator->showMenuWindow();
-            coordinator->schedule->hide();
-        });
-    }*/
     myWindow3->show();
 }
