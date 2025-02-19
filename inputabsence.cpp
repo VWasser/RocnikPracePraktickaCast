40bad53b1895fccb5810e9dbee72952b3da8bd24
COM\vojta <vojta.wasserbauerp@gmail.com>
2025-02-19 15:32:51 +0100

    inputAbsence layout

--- inputabsence.cpp
+++ inputabsence.cpp
@@ -1,4 +1,33 @@
 #include "inputabsence.hpp"
+#include "coordinator.hpp"
 
-inputAbsence::inputAbsence(QWidget *parent): QWidget(parent) {}
+extern Coordinator *coordinator;
+
+inputAbsence::inputAbsence(QWidget *parent): QWidget(parent) {
+
+    QObject::connect(addAbsence, &QPushButton::clicked,this, [](){
+        coordinator->showSchedule();
+    });
+
+    setLayout(mainLayout);
+    nameLine->addWidget(nameLabel);
+    nameLine->addWidget(nameBox);
+
+    dateLine->addWidget(dateLabel);
+    dateLine->addWidget(dateBox);
+
+    classLine->addWidget(classLabel);
+    classLine->addWidget(classBox);
+
+    dayAndHourLine->addWidget(dayAndHourLabel);
+    dayAndHourLine->addWidget(dayBox);
+    dayAndHourLine->addWidget(hourBox);
+
+    mainLayout->addLayout(nameLine);
+    mainLayout->addLayout(dateLine);
+    mainLayout->addLayout(classLine);
+    mainLayout->addLayout(dayAndHourLine);
+    mainLayout->addWidget(addAbsence);
+
+}
 inputAbsence::~inputAbsence(){}
--- inputabsence.hpp
+++ inputabsence.hpp
@@ -14,6 +14,26 @@ public:
     inputAbsence(QWidget *parent = nullptr);
     ~inputAbsence();
 private:
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QHBoxLayout *nameLine = new QHBoxLayout;
+    QHBoxLayout *dateLine = new QHBoxLayout;
+    QHBoxLayout *classLine = new QHBoxLayout;
+    QHBoxLayout *dayAndHourLine = new QHBoxLayout;
+
+private:
+    QLabel *nameLabel = new QLabel(inputAbsence::tr("nameOfStudent"));
+    QLabel *dateLabel = new QLabel(inputAbsence::tr("dateOfAbsence"));
+    QLabel *classLabel = new QLabel(inputAbsence::tr("className"));
+    QLabel *dayAndHourLabel = new QLabel(inputAbsence::tr("DayAndHour"));
+
+private:
+    QLineEdit *nameBox = new QLineEdit;
+    QLineEdit *dateBox = new QLineEdit;
+    QLineEdit *classBox = new QLineEdit;
+    QLineEdit *dayBox = new QLineEdit;
+    QLineEdit *hourBox = new QLineEdit;
+
+    QPushButton *addAbsence = new QPushButton(inputAbsence::tr("addAbsence"));
 
 
 };
