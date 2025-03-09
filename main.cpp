8c209ea22efd19438743c36e5275f887c50adeb3
RomanPodymov <podymfrombryansk@yandex.com>
2025-03-09 15:45:38 +0100

    Trying burger

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <QTranslator>
 #include "BackendlessQt/StandardNetworkManager.hpp"
 #include "coordinator.hpp"
+#include "burger-menu/src/burgermenu.h"
 
 BackendlessAPI* api;
 AnyNetworkAccessManager* networkManager;
 HttpClient* customHttpClient;
 Coordinator* coordinator;
+BurgerMenu* burger;
 
 int main(int argc, char *argv[])
 {
@@ -22,9 +24,14 @@ int main(int argc, char *argv[])
     customHttpClient = new HttpClient();
     networkManager = new StandardNetworkManager();
     api = new BackendlessAPI(networkManager, "7D2C33DB-05E2-4FD9-B26B-46FDB17F56D6", "19CB95DB-0235-4134-B1FB-C64750DE49E2");
-    coordinator = new Coordinator();
+    /*coordinator = new Coordinator();
 
-    coordinator->showMenuWindow();
+    coordinator->showMenuWindow();*/
+
+    burger = new BurgerMenu();
+    burger->addMenuAction("Some");
+    burger->addMenuAction("Other");
+    burger->show();
 
     return myApp.exec();
 }
