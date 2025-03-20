376c96afb2f3f7a579578b3cabb8477f6508d205
RomanPodymov <podymfrombryansk@yandex.com>
2025-03-20 10:50:05 +0100

    Revert "Break"
    This reverts commit 50830bd3402b8ba414a966ccdb1891139e01e3f1.

--- coordinator.cpp
+++ coordinator.cpp
@@ -12,7 +12,7 @@ Coordinator::Coordinator(QObject *parent) : QObject(parent) {
     //abscWin = QSharedPointer<absenceWindow>(new absenceWindow, &QObject::deleteLater);
     absencePopUp = new inputAbsence();
 
-    windows[Screen::SignI] = signInWindow;
+    windows[Screen::SignIn] = signInWindow;
     windows[Screen::Register] = registerWindow;
     windows[Screen::Settings] = popUpWindow;
     windows[Screen::Absence] = abscWin;
